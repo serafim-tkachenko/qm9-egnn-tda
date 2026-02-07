dd7430f114058546d65a492308ac722e0e14cbd4
Serafim Tkachenko <serafim.tkachenko@gmail.com>
2026-02-07 18:22:50 +0000

    Change TDA config to save artidacts to the Google Drive

--- scripts/build_tda_cache.py
+++ scripts/build_tda_cache.py
@@ -2,8 +2,9 @@ from src.data.qm9_data import load_qm9
 from src.data.tda_features import TDACache, TDAConfig
 
 def main():
-    ds = load_qm9("data/qm9")
-    cfg = TDAConfig(cache_dir="artifacts/tda_cache", n_bins=16, max_homology_dim=1)
+    # Here we save everything TDA related to the Google Drive as it's a heavy operation and reruning is costly
+    ds = load_qm9("/content/drive/MyDrive/topo-egnn-qm9/data/qm9")
+    cfg = TDAConfig(cache_dir="/content/drive/MyDrive/topo-egnn-qm9/tda_cache", n_bins=16, max_homology_dim=1)
     tda = TDACache(cfg)
 
     print("TDA feature dim:", tda.feature_dim())
